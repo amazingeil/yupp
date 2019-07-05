a4848abde0bcfb0af271c1c73d4692c1a42ea28e
amazing <chengyuhen@126.com>
2019-07-05 10:11:43 +0800

    第一版

---
+++ pages/air/index.vue
@@ -0,0 +1,16 @@
+<template>
+        <div>
+        买票
+        </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/post/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="container">
+   你好
+  </section>
+</template>
+
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.container{
+  width:1000px;
+  margin:0 auto;
+}
+</style>
